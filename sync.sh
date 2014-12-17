b209151ee147293e3dbca6651cb22f0464347822
Wang Haowei <hwwangwang@gmail.com>
2014-12-18 02:46:44 +0800

    add sync script

---
+++ sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+git pull
+
+function doIt() {
+    rsync --exclude ".vim" --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude ".gitmodules" -av . ~;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in your home directory.
+    Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]];
+    then
+        doIt
+    fi
+fi
+
+unset doIt
+
+#source ~/.zshrc
+
