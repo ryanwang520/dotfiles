25c0ddd81313ccc973115e755bf0f530fe587c94
Wang Haowei <hwwangwang@gmail.com>
2015-01-30 17:46:49 +0800

    update

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ if [ ! -d '$USERNAME/.oh-my-zsh' ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
-if [! $SHELL != /bin/zsh  ]; then
+if [[ $SHELL != /bin/zsh  ]]; then
         sudo chsh -s /bin/zsh $USERNAME
 fi
 
