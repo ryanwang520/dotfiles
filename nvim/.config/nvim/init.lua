87a99a1285408819503773333673e6ba650b54c5
Ryan Wang <hwwangwang@gmail.com>
2023-08-28 11:24:12 +0800

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,7 +23,8 @@ require("lazy").setup({
         {'nvim-telescope/telescope.nvim', tag = '0.1.2', dependencies = { 'nvim-lua/plenary.nvim'},
 	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 	"nvim-lua/plenary.nvim",
-	'nvim-lualine/lualine.nvim'
+	'nvim-lualine/lualine.nvim',
+        'tpope/vim-fugitive'
   }
 })
 
