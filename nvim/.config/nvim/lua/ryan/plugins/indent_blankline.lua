4ecab24cc1dfab3603222c5829c689f4e9d26852
Ryan Wang <hwwangwang@gmail.com>
2023-10-30 21:56:50 +0800

    show file path

--- nvim/.config/nvim/lua/ryan/plugins/indent_blankline.lua
+++ nvim/.config/nvim/lua/ryan/plugins/indent_blankline.lua
@@ -2,12 +2,12 @@ return {
 
   'lukas-reineke/indent-blankline.nvim',
   config = function()
-    require('indent_blankline').setup {
+    require('ibl').setup {
       -- for example, context is off by default, use this to turn it on
-      show_current_context = true,
-      show_current_context_start = true,
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      -- show_current_context = true,
+      -- show_current_context_start = true,
+      -- char = '┊',
+      -- show_trailing_blankline_indent = false,
     }
   end,
 }
--- nvim/.config/nvim/lua/ryan/plugins/lualine.lua
+++ nvim/.config/nvim/lua/ryan/plugins/lualine.lua
@@ -8,6 +8,15 @@ return {
       icons_enabled = true,
       component_separators = '|',
       section_separators = '',
+      sections = {
+        lualine_a = {
+          {
+            'filename',
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
+          }
+        }
+      }
     }
   end,
 }
