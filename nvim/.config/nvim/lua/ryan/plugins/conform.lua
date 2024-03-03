f0b33e6e8a8f893060cfa8f3eb2ef69be9a84a72
Ryan Wang <hwwangwang@gmail.com>
2024-03-03 21:32:28 +0800

    formatting use coform

---
+++ nvim/.config/nvim/lua/ryan/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        rust = { 'rust_fmt' },
+        go = { 'gofmt' },
+        astro = { 'prettier' },
+        terraform = { 'terraform_fmt' },
+        -- Conform will run multiple formatters sequentially
+        python = { 'ruff_fix', 'ruff_format' },
+
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { 'prettierd', 'prettier' } },
+      },
+    }
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*',
+      callback = function(args)
+        require('conform').format { bufnr = args.buf }
+      end,
+    })
+  end,
+}
--- nvim/.config/nvim/lua/ryan/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/ryan/plugins/lspconfig.lua
@@ -18,17 +18,6 @@ return {
     end,
   },
   config = function()
-    require "lspconfig".efm.setup {
-      init_options = { documentFormatting = true },
-      settings = {
-        rootMarkers = { ".git/" },
-        languages = {
-          lua = {
-            { formatCommand = "lua-format -i", formatStdin = true }
-          },
-        }
-      }
-    }
     -- [[ Configure LSP ]]
     --  This function gets run when an LSP connects to a particular buffer.
     local common_on_attach = function(_, bufnr)
@@ -67,11 +56,6 @@ return {
       nmap('<leader>wl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       end, '[W]orkspace [L]ist Folders')
-
-      -- Create a command `:Format` local to the LSP buffer
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-      end, { desc = 'Format current buffer with LSP' })
     end
 
     -- Enable the following language servers
@@ -85,8 +69,6 @@ return {
 
     local servers = {
       clangd = {},
-      terraformls = {},
-      marksman = {},
       gopls = {},
       pyright = {},
       astro = {},
@@ -117,6 +99,7 @@ return {
     mason_lspconfig.setup {
       ensure_installed = vim.tbl_keys(servers),
     }
+
 
     mason_lspconfig.setup_handlers {
       function(server_name)
@@ -168,70 +151,5 @@ return {
     }
 
     rt.setup(opts)
-
-    -- Switch for controlling whether you want autoformatting.
-    --  Use :KickstartFormatToggle to toggle autoformatting on or off
-    local format_is_enabled = true
-    vim.api.nvim_create_user_command('KickstartFormatToggle', function()
-      format_is_enabled = not format_is_enabled
-      print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-    end, {})
-
-    -- Create an augroup that is used for managing our formatting autocmds.
-    --      We need one augroup per client to make sure that multiple clients
-    --      can attach to the same buffer without interfering with each other.
-    local _augroups = {}
-    local get_augroup = function(client)
-      if not _augroups[client.id] then
-        local group_name = 'kickstart-lsp-format-' .. client.name
-        local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-        _augroups[client.id] = id
-      end
-
-      return _augroups[client.id]
-    end
-
-    -- Whenever an LSP attaches to a buffer, we will run this function.
-    --
-    -- See `:help LspAttach` for more information about this autocmd event.
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-      -- This is where we attach the autoformatting for reasonable clients
-      callback = function(args)
-        local client_id = args.data.client_id
-        local client = vim.lsp.get_client_by_id(client_id)
-        local bufnr = args.buf
-
-        -- Only attach to clients that support document formatting
-        if not client.server_capabilities.documentFormattingProvider then
-          return
-        end
-
-        -- Tsserver usually works poorly. Sorry you work with bad languages
-        -- You can remove this line if you know what you're doing :)
-        -- if client.name == 'tsserver' then
-        --   return
-        -- end
-
-        -- Create an autocmd that will run *before* we save the buffer.
-        --  Run the formatting command for the LSP that has just attached.
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          group = get_augroup(client),
-          buffer = bufnr,
-          callback = function()
-            if not format_is_enabled then
-              return
-            end
-
-            vim.lsp.buf.format {
-              async = false,
-              filter = function(c)
-                return c.id == client.id
-              end,
-            }
-          end,
-        })
-      end,
-    })
   end,
 }
