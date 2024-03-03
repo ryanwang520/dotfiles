f7cf821322468667a736bc07ca127da4504d8d44
Ryan Wang <hwwangwang@gmail.com>
2024-03-03 21:48:37 +0800

    typo

--- nvim/.config/nvim/lua/ryan/plugins/conform.lua
+++ nvim/.config/nvim/lua/ryan/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
     require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
-        rust = { 'rust_fmt' },
+        rust = { 'rustfmt' },
         go = { 'gofmt' },
         astro = { 'prettier' },
         terraform = { 'terraform_fmt' },
