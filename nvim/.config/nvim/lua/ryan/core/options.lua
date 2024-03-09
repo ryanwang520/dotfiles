c0e088d543449f4b35d90ce9f2b0756bee77510c
Ryan Wang <hwwangwang@gmail.com>
2024-03-09 17:47:59 +0800

    config fold

--- nvim/.config/nvim/lua/ryan/core/options.lua
+++ nvim/.config/nvim/lua/ryan/core/options.lua
@@ -43,14 +43,13 @@ vim.o.termguicolors = true
 -- curor blink
 vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
 
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.o.foldenable = false
-
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = 'python',
---   callback = function()
---     vim.wo.foldmethod = 'indent'
---     vim.wo.foldlevel = 99
---   end,
--- })
+vim.api.nvim_create_autocmd('FileType', {
+  -- pattern = 'python',
+  callback = function()
+    -- vim.wo.foldmethod = 'indent'
+    -- vim.wo.foldlevel = 99
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.o.foldenable = false
+  end,
+})
